381551c3db88bb468ab6a04e5c0dccb9ca231056
yingxuexiao <1377480989@qq.com>
2023-08-20 19:06:40 +0800

    om

--- R/chapter_7_practice.R
+++ R/chapter_7_practice.R
@@ -1,63 +1,105 @@
 
-#quality check on sequencing reads
-library(ShortRead)
-library(BiocParallel)
-
+##quality check on sequencing reads
 #预准备确定文件具体位置
-setwd("C:/Users/11/Rwork/scRNA-seq/bigdata/fastq/E-MTAB-1147/")
-dirPath <- "C:/Users/11/Rwork/scRNA-seq/bigdata/fastq/E-MTAB-1147" 
+dirPath <- "C:/Users/Administrator/Documents/GitHub/LearningRNA-seq/data/E-MTAB-1147" 
 
+#A  利用rqc()抓取对应的fastq文件并且得到质量检测报告html版
 
-#A  利用rqc()抓取对应的fastq file 
-library(BiocManager)
-BiocManager::install("Rqc")
 library(Rqc)
-
 qcRes=rqc(path=dirPath,pattern=".fastq.gz",openBrowser = FALSE)
+#得到html报告：'C:\Users\Administrator\AppData\Local\Temp\
+#RtmpM5vw6e/rqc_report.html' has been created.
 
-#B  Per base sequence quality boxplot.
+#B 利用对应的函数得到报告中对应的数据
+# 1.获取单碱基测序的质量报告，帮助考虑是否Trim得分低于20的reads，
+#也能帮助检测是否存在接头污染
 rqcCycleQualityBoxPlot(qcRes)
 
-#C  Percentage of nucleotide bases per position
+# 2.获取每个位置核苷酸碱基的百分比的图像
 rqcCycleBaseCallsLinePlot(qcRes)
 
-#D  Check the percent of different duplication levels in FASTQ files
+#D  检查FASTQ文件中不同重复层次的百分比，一般的reads只复制一次
 rqcReadFrequencyPlot(qcRes)
 
 
 
 
+##较为广泛进行测序质量控制的工具为FASTQC，R中使用fastqcr
+library(fastqcr)
+#由于不是mac linux系统，所以需进行操作，让fastqcr用于windows系统：
+.check_if_unix <<- function() {
+  return(NULL)
+}
 
-#filter and trim raeds
-#读取一个fastq文件，并过滤每个质量分数低于20的读数。
+assignInNamespace(".check_if_unix", .check_if_unix, ns = "fastqcr")
 
+wsl.exe /path_to_wsl_fastqc_install/fastqc
+
+fastqcr::fastqc("fastq_path", fastqc.path = "wsl.exe /path_to_wsl_fastqc_install/fastqc")
+
+#安装fastqcr相关的java工具
+fastqc_install()
+
+#获取处理后的结果并将其放在对应的文件夹中(新创一个叫results的文件夹)
+fastqc(fq.dir=dirPath,qc.dir="fastqc_results")
+
+#进行fastqc分析，读取已处理的报告
+qc_report(qc.path="fastqc_results",
+          result.file = "reportFile",preview = TRUE)
+
+qc <- qc_read("fastqc_results/ERR127302_1_subset.fastq.gz")
+qc
+
+qc_plot(qc,"Per base sequence quality")
+
+
+
+
+#####过滤、去除reads
+library(QuasR)
+#获取fastq 文件的路径
+fastqFiles <- system.file(package = "ShortRead",
+                          "extdata/E-MTAB-1147",
+                          c("fastqc_results/ERR127302_1_subset.fastq.gz",
+                            "fastqc_results/ERR127302_2_subset.fastq.gz"))
+#
+outfiles <- paste(tempfile(pattern=c("processed_1_","processed_2_")),
+                  ".fastq",sep="")
+preprocessReads(fastqFiles,outfiles,nBases = 1,
+                truncateStartBases = 3,
+                Lpattern = "ACCCGGGA",
+                minLength = 40)
+#报错 找不到处理过的数据文件，并且最后进行过滤也报错。
+
+
+
+
+####利用ShortRead函数进行数据处理：读取一个fastq文件，并过滤每个质量分数低于20的读数。
 library(ShortRead)
 #A 读取fastq文件
-fastqFile <-"C:/Users/11/Rwork/scRNA-seq/bigdata/fastq/ERR127302_1_subset.fastq.gz" 
+fastqFile <-system.file(package = "ShortRead","extdata/E-MTAB-1147",
+                        "ERR127302_1_subset.fastq.gz") 
 
-#B  read fastq file
+#B 读取fastq file
 fq <- readFastq(fastqFile)
 
-#C  get quality scores per base as a matrix
+#C  利用矩阵得到每个碱基的质量得分
 qPerBase = as(quality(fq), "matrix")
 
-#D  get number of bases per read that have quality score below 20
+#D  获取碱基质量得分<20的数量
 qcount = rowSums( qPerBase <= 20)
 
-#E  Number of reads where all Phred scores >= 20
+#E  得到菲尔德质量评分>=20的reads 数量
 fq[qcount == 0]
 
-#F  write out fastq file with only reads where all quality
-#   scores per base are above 20
+#F  读写出每个碱基中所有read得分高于20的fastq 文件
 writeFastq(fq[qcount == 0],
            paste(fastqFile, "Qfiltered", sep="_"))
 
 #G  fastq文件很大，常用的方法是通过片段化逐条读取
-## set up streaming with block size 1000
+## set up streaming with block size 1000，帮助我们顺利读取片段化的reads
 f <- FastqStreamer(fastqFile,readerBlockSize=1000)
 
-#H   we set up a while loop to call yield() function to
-# go through the file
 while(length(fq <- yield(f))) {
   qPerBase = as(quality(fq), "matrix")
   qcount = rowSums( qPerBase <= 20)
@@ -65,33 +107,30 @@ while(length(fq <- yield(f))) {
              paste(fastqFile, "Qfiltered", sep="_"),
              mode="a")
 }
+#将ShortRead每一步的过程合起来，设置While循环，调用yield()来遍历所有片段化文件。
+#去除<20 reads的基因。
 
 
 
 
-#Mapping/aligning reads to the genome举例
+####映射Mapping/aligning reads to the genome举例
 library(QuasR)
 
-#A  copy example data to current working directory
+#A  复制示例的数据到当前的工作目录路径
+#文件本身在QuasR包中，从C:/program file放至当前目录dirPath中
 file.copy(system.file(package="QuasR", "extdata"),
           ".", recursive=TRUE)
 
-#B  genome file in fasta format
+#B  从文件中打开hg19sub.fa并命名
 genomeFile <- "extdata/hg19sub.fa"
 
-#C  text file containing sample names and fastq file paths
+#C  样本文件中包含了实例名称和fastq文件路径
 sampleFile <- "extdata/samples_chip_single.txt"
 
-#D create alignments
+#D  
 proj <- qAlign(sampleFile, genomeFile)
+proj
 
 
-#当fastqcr需要用MAC|Linux时，需要采用以下算法帮助fastqcr使用。
-.check_if_unix <<- function() {
-  return(NULL)
-}
-assignInNamespace(".check_if_unix", .check_if_unix, ns = "fastqcr")
 
-wsl.exe /path_to_wsl_fastqc_install/fastqc
 
-fastqcr::fastqc("fastq_path", fastqc.path = "wsl.exe /path_to_wsl_fastqc_install/fastqc")
--- R/chapter_8_practice .R
+++ R/chapter_8_practice .R
@@ -28,10 +28,10 @@ colSums(cpm)
 
 ###########Computing RPKM
 
-#A.create a vector of gene lengths
+#A.创建一个基因长度的矩阵
 
 geneLengths <- as.vector(subset(counts,select=c(width)))
-
+#计算RPKM
 
 rpkm <- apply(X =subset(counts,select=c(-width)),
               MARGIN=2,
@@ -42,9 +42,11 @@ colSums(rpkm)
 
 ########Computing TPM
 
+#查找基因长度归一化值
 rpk <- apply(subset(counts,select=c(-width)),2,
              function(x)x/(geneLengths/1000))
 
+#按照基因大小进行RPK标准化
 tpm <- apply(rpk,2,function(x)x/sum(as.numeric(x))*10^6)
 
 tpm
@@ -52,40 +54,43 @@ tpm
 colSums(tpm)
 
 
-
 ###clustering 
 #A.数据方差分析
 v <- apply(tpm,1,var)
 v
 
-#B. selecting genes
+#B.利用标准方差整理结果及降序命令挑选前100的基因
 selectedgenes <- names(v[order(v,decreasing=T)][1:100])
 selectedgenes
 
-#C. produce a heatmap
+#C. 将结果绘制成热图
 install.packages("pheatmap")
 library(pheatmap)
 pheatmap(tpm[selectedgenes,],scale='row',show_rownames = FALSE)
 
+#D. 将结果coldata_file绘制成热图
 coldata <- read.table(coldata_file,header=T,sep='\t',stringsAsFactors = TRUE)
+pheatmap(tpm[selectedgenes,],scale='row',show_rownames = FALSE)
 
-
-######PCA
+######PCA主成分分析
 library(stats)
 library(ggplot2)
 
-#A. transpose the matrix
+#A. 将得到的数据矩阵进行转置（转置矩阵）
 M <- t(tpm[selectedgenes,])
 
+#将数据进行对数转换
 M <- log2(M+1)
-#B. compute PCA
+
+#B. 计算主成分分析
 pcaResults <- prcomp(M)
 
+#利用ggplot2中的autoplot进行主成分分析可视化
 autoplot(pcaResults,data=coldata,colour=('group'))
 
 summary(pcaResults)
 
-####correlation plots
+##相关的Plot
 library(stats)
 correlationMatrix <- cor(tpm)
 
@@ -97,11 +102,12 @@ corrplot(correlationMatrix,order='hclust',
 
 library(pheatmap)
 
+##根据聚类相似度将聚类分成两个
 pheatmap(correlationMatrix,
          annotation_col = coldata,
          cutree_cols=2)
 
-##### differential expression analysis practicing
+##### 差异性分析：
 
 countData <- as.matrix(subset(counts,select=c(-width)))
 
@@ -110,4 +116,72 @@ colData <- read.table(coldata_file,header=T,sep='\t',
 
 designFormula <- "~group"
 
-library()
+#进行DESeq 进行基因差异性表达分析
+library(DESeq2)
+library(stats)
+
+#获取一个有关于以上获得的count data及coldata的DESeq数据集
+dds <- DESeqDataSetFromMatrix(countData=countData,
+                              colData = colData,
+                              design=as.formula(designFormula))
+
+#打印dds查看其内容
+print(dds)
+
+#将基因中的read进行筛选，将read数少于1的移除
+dds <- dds[rowSums(DESeq2::counts(dds))>1,]
+
+#利用DESeq处理数据
+dds <- DESeq(dds)
+
+#计算差异性，其中“CTRL”作为对照组
+DEresults=results(dds,contrast = c("group",'CASE','CTRL'))
+
+#通过提高p值来整理结果
+DEresults <- DEresults[order(DEresults$pvalue),]
+print(DEresults)
+
+###诊断性图像：诊断性检测帮助提升数据的质量、实验系统
+#利用MA plot进行诊断检测，用于观察标准化结果是否良好：大部分散点集中于0轴
+library(DESeq2)
+DESeq2::plotMA(object=dds,ylim=c(-5,5))
+
+##p值分布
+#利用图像观察P值分布，观察到峰值和一致的p值分布
+library(ggplot2)
+ggplot(data=as.data.frame(DEresults),aes(x=pvalue))+
+  geom_histogram(bins=100)
+
+##PCA plot
+#我们需要调取标准化的数据进行绘图，并且对不同的实验使用不同颜色观察聚类效果
+library(DESeq2)
+countsNormalized <- DESeq2::counts(dds,normalized=TRUE)
+
+#获取前500的可变基因
+selectedGenes <- names(sort(apply(countsNormalized,1,var),
+                            decreasing=TRUE)[1:500])
+
+plotPCA(countsNormalized[selectedGenes,],
+        col=as.numeric(colData$group),adj=0.5,
+        xlim=c(-0.5,0.5),ylim=c(-0.5,0.6))
+
+#除了使用plotPCA(),还可以选择使用DESeq2::rlog及DESeq2::plotPCA()来绘出PCA的图像。
+rld <- rlog(dds)
+DESeq2::plotPCA(rld,ntop=500,intgroup='group')+
+  ylim(-50,50)+theme_bw()
+
+#除了PCA之外，还可以进行RLE帮助找出还需要进行标准化的数据，快速对原始或者
+#标准化的数据进行诊断，查看是否需要进一步的处理
+
+library(DESeq)
+par(mfrow=c(1,2))
+plotRLE(countData,outline=FALSE,ylim=c(-4,4),
+        col=as.numeric(colData$group),
+        main='Raw Counts')
+plotRLE(DESeq2::counts(colData$group),
+        mian='Normalized Counts')
+
+
+
+
+
